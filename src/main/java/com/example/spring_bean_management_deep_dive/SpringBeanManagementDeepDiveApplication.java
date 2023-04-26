6413be9f0ed7988af336a3fdcedcde8e2b93ecc4
myelhome <rgty2000@mail.ru>
2023-04-26 21:58:56 +0400

    init commit

---
+++ src/main/java/com/example/spring_bean_management_deep_dive/SpringBeanManagementDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_bean_management_deep_dive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanManagementDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeanManagementDeepDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_bean_management_deep_dive/SpringBeanManagementDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_bean_management_deep_dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeanManagementDeepDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
